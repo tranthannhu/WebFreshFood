0df665b107381768e4743b2ff4f99165a554086f
Trần Thanh Như <65297138+tranthannhu@users.noreply.github.com>
2022-06-08 10:52:51 +0700

    fix connection db pgsql

--- config/database.php
+++ config/database.php
@@ -16,8 +16,8 @@ return [
     */
 
     // 'default' => env('DB_CONNECTION', 'mysql'),
-    'default' => 'postsql',
-    // 'default' => env('DB_CONNECTION', 'pgsql'),
+//     'default' => 'postsql',
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,34 +65,34 @@ return [
             ]) : [],
         ],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'url' => 'postgres://ghpwhkuzdczmkq:586d8c28f8725ce5235bf98ac83687d52340f015157f10d7ed9daf675e6b9a3d@ec2-34-198-186-145.compute-1.amazonaws.com:5432/d71eh8nvs25fqg',
-            'host' => 'ec2-34-198-186-145.compute-1.amazonaws.com',
-            'port' => '5432',
-            'database' => 'd71eh8nvs25fqg',
-            'username' => 'ghpwhkuzdczmkq',
-            'password' => '586d8c28f8725ce5235bf98ac83687d52340f015157f10d7ed9daf675e6b9a3d',
-            'charset' => 'utf8',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
 //         'pgsql' => [
 //             'driver' => 'pgsql',
-//             'url' => env('DATABASE_URL'),
-//             'host' => env('DB_HOST', '127.0.0.1'),
-//             'port' => env('DB_PORT', '5432'),
-//             'database' => env('DB_DATABASE', 'forge'),
-//             'username' => env('DB_USERNAME', 'forge'),
-//             'password' => env('DB_PASSWORD', ''),
+//             'url' => 'postgres://ghpwhkuzdczmkq:586d8c28f8725ce5235bf98ac83687d52340f015157f10d7ed9daf675e6b9a3d@ec2-34-198-186-145.compute-1.amazonaws.com:5432/d71eh8nvs25fqg',
+//             'host' => 'ec2-34-198-186-145.compute-1.amazonaws.com',
+//             'port' => '5432',
+//             'database' => 'd71eh8nvs25fqg',
+//             'username' => 'ghpwhkuzdczmkq',
+//             'password' => '586d8c28f8725ce5235bf98ac83687d52340f015157f10d7ed9daf675e6b9a3d',
 //             'charset' => 'utf8',
 //             'prefix' => '',
 //             'prefix_indexes' => true,
 //             'schema' => 'public',
 //             'sslmode' => 'prefer',
 //         ],
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
