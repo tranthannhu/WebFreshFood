2a77833b98d9ca5368c258c8198c58c78cfe96fa
= <=>
2022-06-09 17:26:27 +0700

    fix cors

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,9 @@ use Illuminate\Http\Request;
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
+Route::get('check', function(){
+    echo "ok";
+});
 
 Route::post('login', 'AuthController@login');
 Route::post('signup', 'AuthController@signup');
