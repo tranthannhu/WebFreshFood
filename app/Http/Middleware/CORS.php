05d8e1cea1901822cd90500bc974b36b32e649f8
= <=>
2022-06-09 16:29:26 +0700

    fix cors

--- app/Http/Middleware/CORS.php
+++ app/Http/Middleware/CORS.php
@@ -15,7 +15,7 @@ class CORS
      */
     public function handle($request, Closure $next)
     {   
-        header('Access-Control-Allow-Origin: *, http://localhost:3000, *localhost*'); // * means any domain
+        header('Access-Control-Allow-Origin: *, http://localhost:3000, *localhost:* ,*localhost*'); // * means any domain
         header('Access-Control-Allow-Headers: Content-type, X-Auth-Token, Authorization, Origin, X-CSRF-Token');
         header('Access-Control-Allow-Methods: *');
         header('Access-Control-Allow-Credentials: true');
