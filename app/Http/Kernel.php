84938975f63470402b01619aab95f4aa1bbf989d
= <=>
2022-06-09 17:21:35 +0700

    fix cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,7 +19,8 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-        \App\Http\Middleware\CORS::class, // added for cross origin
+        // \App\Http\Middleware\CORS::class, // added for cross origin
+        \Fruitcake\Cors\HandleCors::class,
     ];
 
     /**
